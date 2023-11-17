00916725bf4110fb739bba6e2602acf204020c11
Undeemiss <jade@hewittfamily.com>
2023-11-17 15:06:09 -0600

    idk what to call this I'm just due for a commit

--- experienceAnalysis.R
+++ experienceAnalysis.R
@@ -4,17 +4,23 @@ library(scales)
 options(device = "x11")
 
 # Prepare the data
-data <- read.table("data.csv", header = TRUE, sep = ",")
+data <- read.table("data/5e.csv", header = TRUE, sep = ",")
 data$delta <- diff(c(0, data$exp))
+data$log_level <- log(data$level)
+data$log_exp <- log(data$exp)
+data$log_delta <- log(data$delta)
 
 # Fit models against the data
-model_exp <- lm(exp ~ level + I(level^2), data = data)
+model_exp_quadratic <- lm(exp ~ level + I(level^2) + 0, data = data)
+print(summary(model_exp_quadratic))
 
+model_exp_exponential <- lm(log_exp ~ log_level, data = data)
+print(summary(model_exp_exponential))
 
 # Plot exp against level linearly
 plot_exp <- ggplot(data, aes(x = level)) +
-  geom_line() + # Line plot
-  geom_point(aes(y = exp)) + # Add points
+  geom_point(aes(y = exp)) + # Add exact datapoints
+  geom_line(aes(y = predict(model_exp_quadratic, data))) +
   scale_y_continuous(labels = scales::comma) +
   labs(
     title = "Experience Points by Level",
@@ -23,25 +29,8 @@ plot_exp <- ggplot(data, aes(x = level)) +
   )
 
 # Save the plot
-ggsave("plot_exp.png",
+ggsave("output/plot_exp.png",
   plot = plot_exp, width = 15, height = 5, dpi = 300
-)
-
-
-# Plot delta against level linearly
-plot_delta <- ggplot(data, aes(x = as.numeric(row.names(data)), y = delta)) +
-  geom_line() + # Line plot
-  geom_point() + # Add points
-  scale_y_continuous(labels = scales::comma) +
-  labs(
-    title = "Experience Point Delta by Level",
-    x = "Level",
-    y = "Delta"
-  )
-
-# Save the plot
-ggsave("plot_delta.png",
-  plot = plot_delta, width = 15, height = 5, dpi = 300
 )
 
 print(data)

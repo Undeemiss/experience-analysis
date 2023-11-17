02a27810508254b5966bcfc4635aa7429ddef45d
Undeemiss <jade@hewittfamily.com>
2023-11-17 15:25:19 -0600

    Better, I think

--- experienceAnalysis.R
+++ experienceAnalysis.R
@@ -6,31 +6,48 @@ options(device = "x11")
 # Prepare the data
 data <- read.table("data/5e.csv", header = TRUE, sep = ",")
 data$delta <- diff(c(0, data$exp))
-data$log_level <- log(data$level)
-data$log_exp <- log(data$exp)
-data$log_delta <- log(data$delta)
 
 # Fit models against the data
-model_exp_quadratic <- lm(exp ~ level + I(level^2) + 0, data = data)
-print(summary(model_exp_quadratic))
+model_exp <- lm(exp ~ level + I(level^2) + 0, data = data)
+print(summary(model_exp))
+model_delta <- lm(delta ~ level + I(level^2) + 0, data = data)
+print(summary(model_delta))
 
-model_exp_exponential <- lm(log_exp ~ log_level, data = data)
-print(summary(model_exp_exponential))
 
 # Plot exp against level linearly
 plot_exp <- ggplot(data, aes(x = level)) +
   geom_point(aes(y = exp)) + # Add exact datapoints
-  geom_line(aes(y = predict(model_exp_quadratic, data))) +
+  geom_line(aes(y = predict(model_exp, data), color = "red")) +
   scale_y_continuous(labels = scales::comma) +
   labs(
     title = "Experience Points by Level",
     x = "Level",
     y = "Experience Points"
-  )
+  ) +
+  guides(color = "none")
 
 # Save the plot
 ggsave("output/plot_exp.png",
   plot = plot_exp, width = 15, height = 5, dpi = 300
 )
+
+
+# Plot exp against level linearly
+plot_delta <- ggplot(data, aes(x = level)) +
+  geom_point(aes(y = delta)) + # Add exact datapoints
+  geom_line(aes(y = predict(model_delta, data), color = "red")) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(
+    title = "Experience Point Delta by Level",
+    x = "Delta",
+    y = "Experience Points"
+  ) +
+  guides(color = "none")
+
+# Save the plot
+ggsave("output/plot_delta.png",
+  plot = plot_delta, width = 15, height = 5, dpi = 300
+)
+
 
 print(data)
